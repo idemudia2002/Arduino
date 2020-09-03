4ea8d5d97ed3703972840e534816f9134cd393ec
idemudia2002 <idde19@yahoo.com>
2020-09-03 11:30:19 +0200

    measure

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
 void setup() {
+ Serial.begin(9600);
+ Serial.println("starting...");
+ delay(1600);
+
   // put your setup code here, to run once:
 }
 
 void loop() {
+ int value = analogRead(A0);
+ Serial.println(value);
+ 
+ delay(1000);
   // put your main code here, to run repeatedly:
 }
